c581a5573acb2244d1b9d8a11b3a1f30667728c6
sunkink29 <calebparks29@gmail.com>
2017-07-17 12:50:23 -0500

    changed shape reqirements Changed shapes from having to supply and cache the vao and programID to having to just supply the model data from the model class

--- src/main/java/defaultShapes/Shape.java
+++ src/main/java/defaultShapes/Shape.java
@@ -2,7 +2,28 @@ package defaultShapes;
 
 public abstract class Shape {
 	
-	public abstract int getVAO();
-	public abstract int getProgramId();
-
+	abstract int[] getModelData();
+	
+	int vao = -1;
+	int programId = -1;
+	
+	void updateModelData() {
+		int[] shape = getModelData();
+		vao = shape[0];
+		programId = shape[1];
+	}
+	
+	public int getVAO() {
+		if (vao == -1) {
+			updateModelData();
+		}
+		return vao;
+	}
+	
+	public int getProgramId() {
+		if (vao == -1) {
+			updateModelData();
+		}
+		return programId;
+	}
 }
